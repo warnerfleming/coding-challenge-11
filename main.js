af1e0be7c27056a11a4da4d00acb151baf04fca6
warnerfleming <fleming14@usf.edu>
2024-10-27 22:11:00 -0400

    Add event listener for product selection and quantity input

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+//Task 2
+const productSelector = document.getElementById("product-selector")
+const quantityElement = document.getElementById("quantity")
 
+productSelector.addEventListener("change", (event)=>{
+const selectPrice = event.target.value;
+quantityElement.textContent = `$${selectPrice}`
+
+}
+)
